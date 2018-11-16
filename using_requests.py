8b6dc99059c6b6f2e1a33a9806b2f48d03e42b1b
Umair Salam <umair.salam@gmail.com>
2018-11-17 00:45:06 +0500

    Added a comment

--- using_requests.py
+++ using_requests.py
@@ -1,5 +1,7 @@
 import requests
 
+#using requests to pull down web data
+
 url = "https://query1.finance.yahoo.com/v7/finance/download/MU?period1=1525031954&period2=1527623954&interval=1d&events=history&crumb=WLAaf9FVqwT"
 
 url2 = "http://www.umairsalam.com/blog/wp-content/uploads/2018/05/MU.csv"
